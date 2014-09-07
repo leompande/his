f412411048d33e56961cc773850e79522b0205d7
leompande <leo.august27@gmail.com>
2014-09-07 10:29:31 +0300

    adding operation all modules

---
+++ app/models/CategoryFacilities.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mpande
+ * Date: 9/7/14
+ * Time: 2:52 AM
+ */
+
+class CategoryFacilities {
+
+} 
--- app/models/RoomCategory.php
+++ app/models/RoomCategory.php
@@ -2,10 +2,10 @@
 /**
  * Created by PhpStorm.
  * User: mpande
- * Date: 8/8/14
- * Time: 9:21 AM
+ * Date: 9/7/14
+ * Time: 1:41 AM
  */
 
-class Category {
+class RoomCategory {
 
 } 
